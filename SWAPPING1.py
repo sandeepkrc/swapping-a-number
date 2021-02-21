e8e4ba33c9f99f295ad7b6b26755b8c8c92c205b
sandeep kumar chaurasia <58679551+sandeepkrc@users.noreply.github.com>
2021-02-21 11:29:27 +0530

    Add files via upload

---
+++ SWAPPING1.py
@@ -0,0 +1,21 @@
+
+#without third variable
+a=int(input("enter a number=  "))
+b=int(input("enter a second number=  "))
+a=a+b
+b=a-b
+a=a-b
+print("first no is =",a)
+print("second no is  =",b)
+
+#with third variable
+print("again swapping with third variable")
+print("a=",a)
+print("b=",b)
+c=a
+a=b
+b=c
+
+print("a",a)
+print("b",b)
+
